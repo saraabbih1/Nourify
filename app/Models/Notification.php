3eb748535daa417f4eb3ae9895f3b52e83854f9a
saraabbih1 <abbihsara30@gmail.com>
2026-03-23 02:20:48 +0100

    feat:notification model

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    protected $fillable = [
+        'message',
+        'lu',
+        'user_id'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
