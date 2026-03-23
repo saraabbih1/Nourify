089693e6ee1d69eabbc3dfaac4ab711a049e58b1
saraabbih1 <abbihsara30@gmail.com>
2026-03-23 02:19:46 +0100

    feat:don model

--- app/Models/Don.php
+++ app/Models/Don.php
@@ -1,10 +1,24 @@
 <?php
 
-namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Don extends Model
 {
-    //
+    protected $fillable = [
+        'montant',
+        'type',
+        'statut',
+        'donateur_id',
+        'campagne_id'
+    ];
+
+    //  Relations
+
+    public function donateur() {
+        return $this->belongsTo(User::class, 'donateur_id');
+    }
+
+    public function campagne() {
+        return $this->belongsTo(Campagne::class);
+    }
 }
