566c6c690b78f01b5ad97f887aa5084cea534258
saraabbih1 <abbihsara30@gmail.com>
2026-03-23 02:18:58 +0100

    feat campagne model

--- app/Models/Campagne.php
+++ app/Models/Campagne.php
@@ -1,10 +1,25 @@
 <?php
 
-namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Campagne extends Model
 {
-    //
+    protected $fillable = [
+        'titre',
+        'description',
+        'objectif',
+        'montant_collecte',
+        'statut',
+        'beneficiaire_id'
+    ];
+
+    // relations
+
+    public function beneficiaire() {
+        return $this->belongsTo(User::class, 'beneficiaire_id');
+    }
+
+    public function dons() {
+        return $this->hasMany(Don::class);
+    }
 }
