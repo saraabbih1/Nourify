d342a7d006c069798aa6c28bba02a7151f8fda7a
saraabbih1 <abbihsara30@gmail.com>
2026-03-23 02:21:40 +0100

    feat:historiqueaction model

--- app/Models/HistoriqueAction.php
+++ app/Models/HistoriqueAction.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class HistoriqueAction extends Model
 {
-    //
+    protected $fillable = [
+        'action',
+        'user_id'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
