8178ef99f6f2ae71dccd5908b73c4739299852a0
saraabbih1 <abbihsara30@gmail.com>
2026-04-17 12:21:49 +0100

    modifie:routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CampagneController;
+use App\Http\Controllers\DonController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
+use App\Models\Don;
+use App\Models\Notification;
+use App\Models\HistoriqueAction;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,11 +24,45 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/my-notifications', [UserController::class, 'myNotifications']);
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/dons', function () {
+        $dons = Don::latest()->get();
+        return view('dons.index', compact('dons'));
+    })->name('dons.index');
+
+    Route::get('/dons/create', function () {
+        $campagnes = \App\Models\Campagne::latest()->get();
+        return view('dons.create', compact('campagnes'));
+    })->name('dons.create');
+
+    Route::get('/notifications', function () {
+        $notifications = Notification::where('user_id', auth()->id())->latest()->get();
+        return view('notifications.index', compact('notifications'));
+    })->name('notifications.index');
+
+    Route::get('/historique', function () {
+        $historiques = HistoriqueAction::latest()->get();
+        return view('historique.index', compact('historiques'));
+    })->name('historique.index');
+
+    Route::get('/admin', function () {
+        $usersCount = User::count();
+        $campaignsCount = \App\Models\Campagne::count();
+        $donsCount = Don::count();
+        return view('admin.index', compact('usersCount', 'campaignsCount', 'donsCount'));
+    })->name('admin.index');
+
+    Route::get('/admin/users', function () {
+        $users = User::latest()->get();
+        return view('admin.users', compact('users'));
+    })->name('admin.users');
 });
+Route::post('/dons', [DonController::class, 'store'])
+    ->middleware('auth')
+    ->name('dons.store');
 Route::resource('campagnes', CampagneController::class);
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth'])
-    ->name('dashboard');
 
 
 require __DIR__ . '/auth.php';
