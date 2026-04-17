9e8256e48205b29293ea9cf866e5fc8ac2c97b88
saraabbih1 <abbihsara30@gmail.com>
2026-04-17 12:20:30 +0100

    update:dashboardcontroller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,40 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Campagne;
-use App\Models\Notification;
 use App\Models\Don;
-use Illuminate\Support\Facades\Auth;
-use App\Models\HistoriqueAction;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $view = $request->query('view', 'campaigns');
-
-        $campaigns = Campagne::with('dons')->latest()->get();
-
-        $notifications = Notification::where('user_id', Auth::id())
-            ->latest()
-            ->get();
-
         $totalCampaigns = Campagne::count();
         $totalDons = Don::count();
-        $donations = Don::all();
         $totalAmount = Don::sum('montant');
-        $historiques = HistoriqueAction::latest()->get();
-        return view('dashboard', compact(
-            'view',
-            'campaigns',
-            'notifications',
+        $totalUsers = User::count();
+
+        return view('dashboard.index', compact(
             'totalCampaigns',
             'totalDons',
-            'donations',
             'totalAmount',
-            'historiques'
-
+            'totalUsers'
         ));
     }
 }
