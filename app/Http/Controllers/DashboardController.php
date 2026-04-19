eaf09280fd8b2afb756bb3b9b268e055e19e7dc7
saraabbih1 <abbihsara30@gmail.com>
2026-04-19 14:02:14 +0100

    update:dashboardcontroller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Campagne;
 use App\Models\Don;
+use App\Models\HistoriqueAction;
 use App\Models\User;
 
 class DashboardController extends Controller
@@ -21,5 +22,25 @@ class DashboardController extends Controller
             'totalAmount',
             'totalUsers'
         ));
+    }
+
+    public function historique()
+    {
+        $historiques = HistoriqueAction::latest()->get();
+        return view('historique.index', compact('historiques'));
+    }
+
+    public function admin()
+    {
+        $usersCount = User::count();
+        $campaignsCount = Campagne::count();
+        $donsCount = Don::count();
+        return view('admin.index', compact('usersCount', 'campaignsCount', 'donsCount'));
+    }
+
+    public function adminUsers()
+    {
+        $users = User::latest()->get();
+        return view('admin.users', compact('users'));
     }
 }
