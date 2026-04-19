5f6bdb223bf07e2357eaa702ca5932c53202077c
saraabbih1 <abbihsara30@gmail.com>
2026-04-19 14:34:12 +0100

    feat:ajouter netification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-public function myNotifications()
-{
-    return Auth::user()->notifications;
-}}
+    public function myNotifications()
+    {
+        return Auth::user()->notifications;
+    }
+
+    public function notificationsIndex()
+    {
+        $notifications = Notification::where('user_id', auth()->id())->latest()->get();
+        return view('notifications.index', compact('notifications'));
+    }
+}
