6c9abd9278c3e1f9cc05c94928b7df83c99c2ce1
saraabbih1 <abbihsara30@gmail.com>
2026-04-19 14:43:05 +0100

    fix:les routes

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\DonController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
-use App\Models\Don;
-use App\Models\Notification;
-use App\Models\HistoriqueAction;
-use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,59 +22,22 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/my-notifications', [UserController::class, 'myNotifications']);
+    Route::get('/notifications', [UserController::class, 'notificationsIndex'])->name('notifications.index');
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/historique', [DashboardController::class, 'historique'])->name('historique.index');
+    Route::get('/admin', [DashboardController::class, 'admin'])->name('admin.index');
+    Route::get('/admin/users', [DashboardController::class, 'adminUsers'])->name('admin.users');
 
-    Route::resource('campagnes', CampagneController::class)->only(['index', 'show']);
-    Route::resource('campagnes', CampagneController::class)
-        ->except(['index', 'show'])
-        ->middleware('role:beneficiaire,admin');
+    Route::resource('campagnes', CampagneController::class);
 
-    Route::get('/dons', function () {
-        $dons = Don::latest()->get();
-        return view('dons.index', compact('dons'));
-    })->name('dons.index');
+    Route::get('/dons', [DonController::class, 'index'])->name('dons.index');
+    Route::get('/dons/create', [DonController::class, 'create'])->name('dons.create');
+    Route::post('/dons', [DonController::class, 'store'])->name('dons.store');
 
-    Route::get('/dons/create', function () {
-        $campagnes = \App\Models\Campagne::latest()->get();
-        return view('dons.create', compact('campagnes'));
-    })->middleware('role:donateur,admin')->name('dons.create');
-
-    Route::post('/dons', [DonController::class, 'store'])
-        ->middleware('role:donateur,admin')
-        ->name('dons.store');
-
-    Route::post('/dons/{id}/accepter', [DonController::class, 'accepter'])
-        ->middleware('role:beneficiaire,admin')
-        ->name('dons.accepter');
-    Route::post('/dons/{id}/refuser', [DonController::class, 'refuser'])
-        ->middleware('role:beneficiaire,admin')
-        ->name('dons.refuser');
-    Route::post('/dons/{id}/distribuer', [DonController::class, 'distribuer'])
-        ->middleware('role:beneficiaire,admin')
-        ->name('dons.distribuer');
-
-    Route::get('/notifications', function () {
-        $notifications = Notification::where('user_id', auth()->id())->latest()->get();
-        return view('notifications.index', compact('notifications'));
-    })->name('notifications.index');
-
-    Route::get('/historique', function () {
-        $historiques = HistoriqueAction::latest()->get();
-        return view('historique.index', compact('historiques'));
-    })->name('historique.index');
-
-    Route::get('/admin', function () {
-        $usersCount = User::count();
-        $campaignsCount = \App\Models\Campagne::count();
-        $donsCount = Don::count();
-        return view('admin.index', compact('usersCount', 'campaignsCount', 'donsCount'));
-    })->middleware('role:admin')->name('admin.index');
-
-    Route::get('/admin/users', function () {
-        $users = User::latest()->get();
-        return view('admin.users', compact('users'));
-    })->middleware('role:admin')->name('admin.users');
+    Route::post('/dons/{id}/accepter', [DonController::class, 'accepter'])->name('dons.accepter');
+    Route::post('/dons/{id}/refuser', [DonController::class, 'refuser'])->name('dons.refuser');
+    Route::post('/dons/{id}/distribuer', [DonController::class, 'distribuer'])->name('dons.distribuer');
 
 });
 
